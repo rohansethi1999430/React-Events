29031463ae998367d82bcb07ae4c1adc555cc657
Rohan <rohansethi1999430@gmail.com>
2021-05-20 17:14:36 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Button from './Button';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <Button/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Button.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class Button extends Component{
+    render(){
+        return (
+            <button onClick={function(){alert("How dare you to click me...")}}>Click me!!</button>
+        )
+    }
+}
+export default Button;
