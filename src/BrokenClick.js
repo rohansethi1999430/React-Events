7958aa6bef18a3b3e8ef534d3eef6b399d3fa2fe
Rohan <rohansethi1999430@gmail.com>
2021-05-22 10:15:36 +0530

    Added BrokenClick Component

---
+++ src/BrokenClick.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+class BrokenClick extends Component{
+    render(){
+        return(
+            <button></button>
+        )
+    }
+}
+export default BrokenClick;
